2a394ef22317a9f991b076d57ef07757aad22e08
Luca Soler <e228154@edu.sbl.ch>
2026-01-16 14:22:13 +0100

    ex-05 text wird bei talentscorre ausgespuckt

--- src/exercises/ex-05.js
+++ src/exercises/ex-05.js
@@ -1,8 +1,20 @@
 const gender = process.argv[2];
 const height = Number(process.argv[3]);
 
+let talent = 0;
+
 if (gender === "m") {
-const talentM = 243 - height;
-} else if (gender === "f") {
-const talentF = 224 - height;
+    talent = (243 - height);
 }
+ else if (gender === "f") {
+    talent = (224 - height);
+}
+
+if ( talent < 50) {
+    console.log(`Du hast sehr viel Potenzial`);}
+else if (talent > 50 && talent < 60) {
+    console.log(`Du musst dringend an deiner Spungkraft arbeiten.`);}
+else if (60 < talent && talent < 70) {
+    console.log(`Wenn du schnell bist, kannst du dir die Libero-Position überlegen.`);}
+else if ( talent > 70) {
+    console.log(`Du wirst im Volleyball nicht glücklich werden.`);}
