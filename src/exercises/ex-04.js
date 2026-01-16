32e9cf2f2816605de456ea578ab0732b8f58068e
Luca Soler <e228154@edu.sbl.ch>
2026-01-16 13:34:41 +0100

    16.1 ex-04 geändert das nicht mehr gröse durch cm^2 gerechnet wird sondern in m^2

--- src/exercises/ex-04.js
+++ src/exercises/ex-04.js
@@ -1,6 +1,6 @@
 const weight = Number(process.argv[2]);
 const height = Number(process.argv[3]);
 
-const bmi = weight / (height * height);
+const bmi = weight / ((height/100) * (height/100));
 
 console.log(`Dein BMI ist ${bmi}.`);
