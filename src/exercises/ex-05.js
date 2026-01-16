4a10ab2b385c1fd4a58f7cd3ed904f0a7463f169
Luca Soler <e228154@edu.sbl.ch>
2026-01-16 13:49:17 +0100

    ex-05 anderer talentscorre wird für geschlecht angepasst

--- src/exercises/ex-05.js
+++ src/exercises/ex-05.js
@@ -1,7 +1,8 @@
 const gender = process.argv[2];
 const height = Number(process.argv[3]);
 
-
-
-const talentM = 243 - height
-const talentF = 224 - height
+if (gender === "m") {
+const talentM = 243 - height;
+} else if (gender === "f") {
+const talentF = 224 - height;
+}
